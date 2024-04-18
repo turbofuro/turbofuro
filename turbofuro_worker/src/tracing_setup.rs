e2ca51e26982d44949dedb6266422c0b709658fc
Bartosz Wiśniewski <bartosz.wisniewski@turbofuro.com>
2024-04-18 23:41:06 +0200

    Add console subscriber setup

--- turbofuro_worker/src/tracing_setup.rs
+++ turbofuro_worker/src/tracing_setup.rs
@@ -3,6 +3,9 @@ use tracing_subscriber::fmt::format::FmtSpan;
 use tracing_subscriber::EnvFilter;
 
 pub fn init() {
+    // If you want to use the console subscriber, you can uncomment this line:
+    // console_subscriber::init();
+
     let _ = tracing_subscriber::fmt()
         .with_env_filter(
             EnvFilter::builder()
