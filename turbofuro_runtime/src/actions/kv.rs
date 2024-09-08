469e69f8add491f35028fbbbca1534ee7c9a937e
Bartosz Wiśniewski <bartosz.wisniewski@turbofuro.com>
2024-09-09 01:37:49 +0200

    Add expiration to KV

--- turbofuro_runtime/src/actions/kv.rs
+++ turbofuro_runtime/src/actions/kv.rs
@@ -7,13 +7,28 @@ use tracing::instrument;
 
 use crate::{
     errors::ExecutionError,
-    evaluations::eval_param,
-    executor::{ExecutionContext, Parameter},
+    evaluations::{eval_optional_param, eval_param},
+    executor::{get_timestamp, ExecutionContext, Parameter},
 };
 
-use super::store_value;
+use super::{as_u64, store_value};
 
-static KV: Lazy<Arc<DashMap<String, StorageValue>>> = Lazy::new(|| Arc::new(DashMap::new()));
+#[derive(Debug, Clone)]
+struct KvValue {
+    value: StorageValue,
+    expiration: Option<u64>,
+}
+
+impl From<StorageValue> for KvValue {
+    fn from(value: StorageValue) -> Self {
+        KvValue {
+            value,
+            expiration: None,
+        }
+    }
+}
+
+static KV: Lazy<Arc<DashMap<String, KvValue>>> = Lazy::new(|| Arc::new(DashMap::new()));
 
 #[instrument(level = "trace", skip_all)]
 pub async fn read_from_store<'a>(
@@ -25,13 +40,21 @@ pub async fn read_from_store<'a>(
     let key_param = eval_param("key", parameters, &context.storage, &context.environment)?;
     let key = key_param.to_string().map_err(ExecutionError::from)?;
 
-    let value = KV
+    let kv_value = KV
         .get(key.as_str())
         .map(|v| v.clone())
-        .unwrap_or(StorageValue::Null(None));
+        .unwrap_or(StorageValue::Null(None).into());
 
-    store_value(store_as, context, step_id, value).await?;
+    if let Some(expiration) = kv_value.expiration {
+        let now = get_timestamp();
+        if now > expiration {
+            KV.remove(key.as_str());
+            store_value(store_as, context, step_id, StorageValue::Null(None)).await?;
+            return Ok(());
+        }
+    }
 
+    store_value(store_as, context, step_id, kv_value.value).await?;
     Ok(())
 }
 
@@ -44,8 +67,20 @@ pub async fn write_to_store<'a>(
     let key_param = eval_param("key", parameters, &context.storage, &context.environment)?;
     let key = key_param.to_string().map_err(ExecutionError::from)?;
 
-    let value_param = eval_param("value", parameters, &context.storage, &context.environment)?;
-    KV.insert(key, value_param);
+    let value = eval_param("value", parameters, &context.storage, &context.environment)?;
+
+    let mut expiration = None;
+    let expiration_param = eval_optional_param(
+        "expiration",
+        parameters,
+        &context.storage,
+        &context.environment,
+    )?;
+    if let Some(expiration_param) = expiration_param {
+        expiration = Some(get_timestamp() + as_u64(expiration_param, "expiration")?);
+    }
+
+    KV.insert(key, KvValue { value, expiration });
 
     Ok(())
 }
